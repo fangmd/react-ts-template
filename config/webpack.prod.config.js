00fb512fdd8898670eb3502a51e8dd87d0aeb508
fangmingdong <fangmingdong@58.com>
2021-04-13 18:49:05 +0800

    save CssMinimizerPlugin

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin");
-
+const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const { merge } = require('webpack-merge')
 const webpackConfigBase = require('./webpack.base.config')
 
 const webpackProdConfig = {
-  mode: "production",
+  mode: 'production',
   entry: {
     app: ['@babel/polyfill', path.join(__dirname, '../src', 'index.tsx')],
   },
@@ -17,7 +17,14 @@ const webpackProdConfig = {
     runtimeChunk: {
       name: 'manifest',
     },
-    minimizer: [new TerserPlugin()], // [new UglifyJsPlugin({...})]
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: 'advanced',
+        },
+      }),
+    ], // [new UglifyJsPlugin({...})]
     splitChunks: {
       chunks: 'async',
       minSize: 30000,
@@ -48,15 +55,13 @@ const webpackProdConfig = {
     filename: 'static/js/[name].[contenthash:8].js',
     path: path.resolve(__dirname, '../build'),
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ],
+  plugins: [new CleanWebpackPlugin()],
 }
 
 const baseConfig = webpackConfigBase('production')
 
 const resultConfig = merge(baseConfig, webpackProdConfig)
 
-console.log(resultConfig);
+console.log(resultConfig)
 
 module.exports = resultConfig
