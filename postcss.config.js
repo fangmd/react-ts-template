f9188676e1f76a3d982ba804e45348864454b686
fangmingdong <fangmingdong0224@gmail.com>
2022-01-09 23:29:10 +0800

    feat: vw

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,14 @@
-module.exports = {
-  plugins: [require('autoprefixer')],
+const path = require('path')
+module.exports = ({ file }) => {
+  const designWidth = file.toString().indexOf('vant') > -1 ? 375 : 750 //根据设计图设置大小适配vant-react
+  return {
+    plugins: [
+      require('autoprefixer'),
+      {
+        'postcss-px-to-viewport': {
+          viewportWidth: designWidth,
+        },
+      },
+    ],
+  }
 }
